291f2bc46c616f3e36a83834da68ca02241f28b4
catter-unprg28 <57850271+catter-unprg28@users.noreply.github.com>
2019-12-07 18:09:54 -0500

    CONDICIONAL MULTIPLES9

---
+++ catter/multiples9.py
@@ -0,0 +1,20 @@
+# ingresar dos edades
+# si el primero es mayor que el segundo
+
+#DECLARACION
+edad1=0
+edad2=0
+
+import os
+
+#INPUT
+edad1=int(os.sys.argv[1])
+edad2=int(os.sys.argv[2])
+
+#PROCESSING
+if (edad1>edad2):
+    print("la primera edad es mayor que la segunda")
+if (edad1==edad2):
+    print("son iguales")
+if (edad1<edad2):
+    print("la segunda es mayor que la primera")
